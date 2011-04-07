c691872f529d5860ad45b43cf708ce3ae28eda2c
Alex Koppel <alex+git@alexkoppel.com>
2011-04-07 10:21:11 +0200

    Wrote out the rest of the plugin.

--- jquery.trafficlight.js
+++ jquery.trafficlight.js
@@ -12,7 +12,10 @@
     {
       selector: "#domSelectorToUpdate",
       url: "http://myURL" || function(resultsFromLastStep) { doStuff(); return "myURL" },
-      [error: stepSpecificErrorHandler]
+      // optional arguments
+      error: stepSpecificErrorHandler,
+      success: stepSpecificSuccessHandler,
+      args: stepSpecificArgsHash
     }
   
 */
@@ -30,6 +33,9 @@
       args: {},
       nextArgs: {}
     },
+    
+    isPaused: false,
+    results: [],
   
     _create: function() {
       var opts = this.options;
@@ -38,8 +44,8 @@
     
       opts.currentStep = opts.startStep || 0;
     
-      if (opts.autostart) {
-        //this.start();
+      if (opts.autostart && opts.steps.length > 0) {
+        this.start();
       }
     },
     
@@ -75,22 +81,111 @@
     
       return this;
     },
+    
+    pause: function() {
+      this.paused = true;
+    },
   
-    proceed: function(lastResults) {
-      // advance to the next step
-      var trafficlight = this.data("trafficlight"),
-          settings = trafficlight.settings;
-          stepIndex = settings.currentStep++,
-          step = settings.steps[stepIndex],
-          destination = step.url;  
-    
-      if (typeof(destination) === "function") {
-        // if they've supplied a function, 
-        destination = destination.apply(this, lastResults)
+    start: function(startStep) {
+      if (startStep) {
+        this.options.currentStep = startStep;
       }
+      
+      this.paused = true;
+      
+      this._proceed();
+    },
+  
+    _proceed: function() {
+      // advance to the next step
+      var options = this.options,
+          currentStep = options.currentStep,
+          stepDetails = options.steps[currentStep],
+          destination, node, data;
+      
+      var lastResults = this.results[currentStep - 1];
+      
+      if (stepDetails) {
+        destination = stepDetails.url;
+        node = $(stepDetails.selector);
+        step.data = $.extend({}, this.options.args || {}, this.options.nextArgs || {}, stepDetails.args || {});  
     
+        if (typeof(destination) === "function") {
+          // if they've supplied a function, call it
+          destination = destination(lastResults, step);
+        }
+    
+        // update the DOM node
+        node.removeClass(options.toDoClass).removeClass(options.doneClass).removeClass(options.failedClass).addClass(options.doingClass);
+      
+        // now make the Ajax call
+        jQuery.ajax(destination, {
+          success: this._callSucceeded,
+          error: this._callErrored,
+          data: step.data,
+          type: stepDetails.method || "get"
+        })
+      }
+      else {
+        throw("Trafficlight told to proceed to step " + currentStep + " but there are no details for it!")
+      }
+    },
+    
+    this._callSucceeded: function(data) {
+      // a call succeeded!  trigger our success functions
+      var options = this.options, step = options.steps[options.currentStep];
+      var successData = {
+        response: data,
+        step: step
+      };
+      
+      // store data and clean up temporary arguments
+      this.results[step] = data;
+      this.options.nextArgs = {};
+      
       // update the DOM node
-      $(step.selector).removeClass(settings.toDoClass).addClass(settings.doingClass);   
+      $(step.selector).removeClass(options.toDoClass).removeClass(options.doingClass).removeClass(options.failedClass).addClass(options.doneClass);      
+      
+      if (typeof(step.success) === "function") {
+        step.success(successData);
+      }
+      
+      this._trigger("success", successData);
+
+      // now, advance to the next step
+      options.currentStep++;
+      
+      // and if we're not paused, proceed
+      if (!this.paused) {
+        this._proceed(data);
+      }
+    },
+    
+    this._callErrored: function(jqXHR, textStatus, errorThrown) {
+      // error!  call the local and global error functions
+      var options = this.options, step = options.steps[options.currentStep];
+      var stepErrorResult, globalErrorResult;
+      var errorData = {
+        text: textStatus,
+        exception: errorThrown,
+        step: step
+      };
+      
+      // call the step-specific and global error handlers
+      if (typeof(step.error) === "function") {
+        stepErrorResult = step.error(errorData)
+      }
+      globalErrorResult = this._trigger("error", errorData);
+      
+      // you can tell the plugin to retry after error by returning "retry"
+      if (stepErrorResult === "retry" || globalErrorResult === "retry") {
+        this._proceed();
+      }
+      else {
+        // otherwise, we pause the app and mark it as failed
+        this.paused = true;
+        $(step.selector).removeClass(options.toDoClass).removeClass(options.doingClass).removeClass(options.doneClass).addClass(options.failedClass);      
+      }
     }
   });
 }(jQuery))
